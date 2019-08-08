78be895588f42c17d3a7d5e3864fee660e400eb4
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-08-08 15:44:03 -0700

    background and player

--- src/index.js
+++ src/index.js
@@ -1,34 +1,96 @@
-cimport 'phaser';
+import 'phaser';
+
 
 var config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
+    parent: 'gameArea',
+	width: window.innerWidth,
+    height: window.innerHeight,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: true
+        }
+    },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
-var game = new Phaser.Game(config);
+
+
+const game = new Phaser.Game(config);
+
 
 function preload ()
 {
-    this.load.image('logo', 'assets/logo.png');
+    
+    this.load.image('background', 'assets/backgrounds/background.png');
+    this.load.spritesheet("player", "assets/dinos/blinky_dino.png", {
+        frameWidth: 24,
+        frameHeight: 21
+    });
+    this.load.image('ground', '/assets/border/border.png');
+
+
+    resize();
 }
 
 function create ()
 {
-    var logo = this.add.image(400, 150, 'logo');
+    // platforms = this.physics.add.staticGroup();
+    // platforms.create(600, 400, 'ground');
 
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
+    var centerY = this.cameras.main.centerY;
+    var centerX = this.cameras.main.centerX
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    
+    var bg = this.add.image(0, 0, 'background');
+    bg.displayWidth = 500;bg.displayHeight = 400;
+    bg.setPosition(centerX, centerY);
+
+    var platforms = this.physics.add.staticGroup();
+    platforms.create(this.cameras.main.centerX, this.cameras.main.centerY + 180, 'ground');
+    let player = this.physics.add.sprite(centerX, centerY, 'player');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.physics.add.collider(player, platforms);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
     });
+}
 
+function update() 
+{
+    if (Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+        player.setVelocityY(-330);
+    }
+
+
+}
+
+function resize(){
+    let canvas = document.querySelector("canvas");
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    let windowRatio = windowWidth / windowHeight;
+    let gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
 }
